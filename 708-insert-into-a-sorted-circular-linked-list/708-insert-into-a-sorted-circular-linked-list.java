7cdbae4bb9a0c9c4a96a111068682f08104c0198
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-06-18 21:57:08 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (44.40%) - LeetHub

---
+++ 708-insert-into-a-sorted-circular-linked-list/708-insert-into-a-sorted-circular-linked-list.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        Node newNode=new Node(insertVal);    
+        if(head==null){
+            newNode.next=newNode;
+            return newNode;
+        }
+        Node prev=head;
+        Node curr=head.next; boolean toInsert=false;
+       do{
+           if(prev.val <=insertVal && insertVal<=curr.val){
+               toInsert=true;
+           }else if(prev.val>curr.val){
+               if(insertVal>=prev.val || insertVal<=curr.val)
+                   toInsert=true;
+           }
+           if(toInsert){
+               prev.next=new Node(insertVal,curr);
+               return head;
+           }
+           prev=curr;
+           curr=curr.next;
+       }while(prev!=head);
+           
+        prev.next=new Node(insertVal,curr);
+        return head;
+        
+    }
+}
