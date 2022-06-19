76c18f51a8c0d82d8ba8cfd5a85afce944333119
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-06-19 12:43:17 +0530

    Time: 18 ms (21.01%), Space: 42.5 MB (86.83%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -9,23 +9,5 @@ class Solution {
                 i++;
             }
         }
-        // int[] result=new int[arr.length];
-        // int k=0;
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]==0){
-        //         result[k]=0;
-        //         k=k+1;
-        //         if(k>=arr.length){
-        //          break;   
-        //         }
-        //         result[k]=0;
-        //     }else{
-        //         result[k]=arr[i];
-        //     }
-        //     k++;
-        //     if(k>=arr.length)
-        //         break;
-        // }
-        // arr=result;
     }
 }
