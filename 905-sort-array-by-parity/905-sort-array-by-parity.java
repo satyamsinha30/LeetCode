3bd89e1da51abda92a6f2acaac051ac4b0e9e50f
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-06-20 20:26:57 +0530

    Time: 1 ms (99.95%), Space: 43.2 MB (90.34%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,14 +1,30 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        Integer[] result=new Integer[nums.length];
-        for (int i = 0; i < nums.length; i++)
-            result[i] = nums[i];
-        
-        Arrays.sort(result,(a, b)->Integer.compare(a%2, b%2));
-        
-        for (int i = 0; i < nums.length; i++)
-            nums[i] = result[i];
+        //Method 3 (O(N) & space:O(1))
+        int i=0,j=nums.length-1;
+        while(i<j){
+            if(nums[i]%2 > nums[j]%2){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            if(nums[i]%2 ==0) i++;
+            if(nums[j]%2 ==1) j--;
+        }
         return nums;
+        
+        
+        
+        //Method 2 (Using custom comparator)
+//         Integer[] result=new Integer[nums.length];
+//         for (int i = 0; i < nums.length; i++)
+//             result[i] = nums[i];
+        
+//         Arrays.sort(result,(a, b)->Integer.compare(a%2, b%2));
+        
+//         for (int i = 0; i < nums.length; i++)
+//             nums[i] = result[i];
+//         return nums;
         
         
         //Method 1 (Trivial Time:O(2N) , Space :O(N))
