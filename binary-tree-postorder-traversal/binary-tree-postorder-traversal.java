cbf1aef1fd486e137128fb2aa2057e2bdcfd680e
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-06-24 00:42:30 +0530

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,15 +14,51 @@
  * }
  */
 class Solution {
-    List<Integer> result=new ArrayList<>();
+    // List<Integer> result=new ArrayList<>();
+    
+    private TreeNode getLeftMostNode(TreeNode leftNode,TreeNode curr){
+        while(leftNode.left!=null && leftNode.left!=curr)
+            leftNode=leftNode.left;
+        return leftNode;
+    }
     public List<Integer> postorderTraversal(TreeNode root) {
-        if(root==null)
-            return result;
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
-        result.add(root.val);
         
+        //Method 2( Using Morris)
+  LinkedList<Integer> result= new LinkedList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            TreeNode rightNode=curr.right;
+            if(rightNode==null){
+                result.add(curr.val);
+                curr=curr.left;
+            }else{
+                TreeNode leftMostNode=getLeftMostNode(rightNode,curr);
+                if(leftMostNode.left==null){
+                    //Thread has to be created
+                    result.add(curr.val);
+                    leftMostNode.left=curr;
+                    curr=curr.right;
+                }else{
+                    //Thread has to be deleted
+                    leftMostNode.left=null;
+                    
+                    curr=curr.left;
+                }
+            }
+        }
+        Collections.reverse(result);;
         return result;
+        
+        
+        
+        
+//         if(root==null)
+//             return result;
+//         postorderTraversal(root.left);
+//         postorderTraversal(root.right);
+//         result.add(root.val);
+        
+//         return result;
         
         
     }
