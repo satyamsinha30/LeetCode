f62d219aedd426713261960185171a6a541f1a5d
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-06-23 20:38:00 +0530

    Time: 1 ms (48.30%), Space: 42.2 MB (56.23%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -14,23 +14,53 @@
  * }
  */
 class Solution {
+    public TreeNode getRightMostNode(TreeNode rightMost,TreeNode curr){
+        while((rightMost.right!=null) && (rightMost.right!=curr))
+            rightMost=rightMost.right;
+        return rightMost;
+    }
     public List<Integer> preorderTraversal(TreeNode root) {
-        LinkedList<TreeNode> stack=new LinkedList<>();
-        LinkedList<Integer> output =new LinkedList<>();
-        if(root== null){
-            return output;
-        }
-        stack.add(root);
-        while(!stack.isEmpty()){
-            TreeNode node=stack.pollLast();
-            output.add(node.val);
-            if(node.right!=null){
-                stack.add(node.right);
+        //Method:2 (Morris Traversal)
+        LinkedList<Integer> ans=new LinkedList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            TreeNode leftNode=curr.left;
+            if(leftNode==null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }else{
+                TreeNode rightMostNode=getRightMostNode(leftNode,curr);
+                if(rightMostNode.right==null){
+                    //Thread needs to be created
+                    ans.add(curr.val);
+                    rightMostNode.right=curr;
+                    curr=curr.left;
+                }else{
+                    //Delete Thread
+                    rightMostNode.right=null;
+                    curr=curr.right;
+                }
             }
-            if(node.left!=null){
-                stack.add(node.left);
-            }
         }
-        return output;
+        return ans;
+        
+        // Method 1( Trivial :Stack)
+        // LinkedList<TreeNode> stack=new LinkedList<>();
+        // LinkedList<Integer> output =new LinkedList<>();
+        // if(root== null){
+        //     return output;
+        // }
+        // stack.add(root);
+        // while(!stack.isEmpty()){
+        //     TreeNode node=stack.pollLast();
+        //     output.add(node.val);
+        //     if(node.right!=null){
+        //         stack.add(node.right);
+        //     }
+        //     if(node.left!=null){
+        //         stack.add(node.left);
+        //     }
+        // }
+        // return output;
     }
 }
