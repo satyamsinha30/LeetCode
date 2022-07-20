732f79051ec1ff44552e591b43adf2f13c9f1e1b
satyamsinha30 <32727587+satyamsinha30@users.noreply.github.com>
2022-07-20 22:39:32 +0530

    Time: 1102 ms (10.86%), Space: 70.7 MB (30.85%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,8 +1,45 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums,0,nums.length);
+        // mergeSort(nums,0,nums.length);
+        quickSort(nums,0,nums.length);
         return nums;
     }
+    
+    //Method: 2(Quick Sort)
+    private void quickSort(int[] nums,int start,int end){
+        if(end-start<2)
+            return;
+        int pivotIndex=partition(nums,start,end);
+        quickSort(nums,start,pivotIndex);
+        quickSort(nums,pivotIndex+1,end);
+    }
+    
+    private int partition(int[] nums,int start,int end){
+        //Taking pivotElement as starting of array.
+        int pivotElement=nums[start];
+        int i=start;
+        int j=end;
+        while(i<j){
+            
+            //Empty loop body
+            while(i<j && nums[--j]>=pivotElement);
+            if(i<j){
+                nums[i]=nums[j];
+            }
+            
+            //Empty loop body
+            while(i<j && nums[++i]<=pivotElement);
+            if(i<j){
+                nums[j]=nums[i];
+            }
+        }
+        nums[j]=pivotElement;
+        return j;
+        
+    }
+    
+    
+    //Method:1 (Merge Sort)
     private void mergeSort(int[] nums,int start,int end){
         if(end-start<2)
             return;
